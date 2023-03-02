d14678b56ab7a12e2cb196cc5175d51e8e6df763
Lean-05 <leandroorderique@gmail.com>
2023-03-01 22:58:49 -0300

    Ejercicio2 Finalizado

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+const fecha = new Date(2003, 3 , 5);
+
+const libroFav = {
+    titulo: "Las ventajas de ser invisible",
+    autor:"Stephen Chbosky",
+    fecha:"1/09/2022",
+    url:"https://www.cuspide.com/Libro/9789870426813/Las+Ventajas+De+Ser+Invisible"
+}
+
+let lista = [
+    "leandro",
+    19,
+    true,
+    fecha,
+    libroFav
+]
